99ea52d2ae3615e3a1fb5eb9947cf4d82812b718
JiaG <giau32002@gmail.com>
2024-01-28 21:32:31 +0700

    done Tuan03

---
+++ 07_20053331_NguyenVanGiau_Tuan03/index.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Invoice_1 = require("./bai05/Invoice");
+var Shape_1 = require("./bai07/Shape");
+var Vehicle_1 = require("./bai08/Vehicle");
+//bai 5
+(0, Invoice_1.mainInvoice)();
+console.log("\n\n----------------------------------------\n\n");
+console.log("\x1b[31m", "# Đọc hướng dẫn trong index.ts để chạy bài 6 #", "\x1b[0m");
+//bai 6
+/* chay bai 6 voi phuong thuc thu cong do cmd nodemon khong the hien thi thong tin console.log()
+
+1.   unBlock mainManagement()
+2.   tsc index.ts
+3.   node index.js
+*/
+// mainManagement() 
+console.log("\x1b[31m", "# Đọc hướng dẫn trong index.ts để chạy bài 6 #", "\x1b[0m");
+console.log("\n\n----------------------------------------\n\n");
+//bai 7
+(0, Shape_1.mainShape)();
+console.log("\n\n----------------------------------------\n\n");
+//bai 8
+(0, Vehicle_1.mainVehicle)();
+console.log("\n\n----------------------------------------\n\n");
---
+++ 07_20053331_NguyenVanGiau_Tuan03/index.ts
@@ -0,0 +1,33 @@
+import { mainInvoice } from "./bai05/Invoice";
+import { mainManagement } from "./bai06/Management";
+import { mainShape } from "./bai07/Shape";
+import { mainVehicle } from "./bai08/Vehicle";
+
+
+//bai 5
+mainInvoice()
+console.log("\n\n----------------------------------------\n\n");
+console.log("\x1b[31m", "# Đọc hướng dẫn trong index.ts để chạy bài 6 #", "\x1b[0m");
+
+//bai 6
+/* chay bai 6 voi phuong thuc thu cong do cmd nodemon khong the hien thi thong tin console.log()
+
+1.   unBlock mainManagement()
+2.   tsc index.ts
+3.   node index.js 
+*/
+
+// mainManagement() 
+console.log("\x1b[31m", "# Đọc hướng dẫn trong index.ts để chạy bài 6 #", "\x1b[0m");
+console.log("\n\n----------------------------------------\n\n");
+
+
+
+//bai 7
+mainShape()
+console.log("\n\n----------------------------------------\n\n");
+
+
+//bai 8
+mainVehicle()
+console.log("\n\n----------------------------------------\n\n");
